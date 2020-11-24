ed30c086df4bcf582b227da56b99410f15532faa
Sana Chan <sanachan@Sanas-MacBook-Pro.local>
2020-11-24 12:25:00 -0800

    [Doesn't work] First attempt at updating the leaderboard

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,21 @@
 import React from "react";
 import leaderboard from "../assets/leaderboard.png";
+import firebase from '../firebase';
 
 class User extends React.Component {
   render() {
+      var top5Users = [];
+      var getTopUsers = firebase.database().ref(`users`)
+        .orderByChild(`totalScore`)
+        .limitToLast(5)
+        .once('value', (snapshot) => {
+          snapshot.forEach((childSnapshot) => {
+            const data = childSnapshot.val();
+            console.log("Exists", data.totalScore);
+            top5Users.push(data.username);
+            console.log("name", data.username);
+          })
+        });
 
       return (
         <div>
@@ -12,8 +25,15 @@ class User extends React.Component {
         </div>
         <ul>
           <b>
-          <li>1. User1</li>
-          <li>2. User2</li>
+          <script>
+            document.getElementById("first").innerHTML = top5Users[4];
+            document.getElementById("second").innerHTML = top5Users[3];
+            document.getElementById("third").innerHTML = top5Users[2];
+            document.getElementById("fourth").innerHTML = top5Users[1];
+            document.getElementById("fifth").innerHTML = top5Users[0];
+          </script>
+          <li>1. <p id="first"></p></li>
+          <li>2. </li>
           <li>3. User3</li>
           <li>4. User4</li>
           <li>5. User5</li>
