71cec5566ab9fc99656c55b557c75d84fd0fa929
Sana Chan <sanachan@Sanas-MacBook-Pro.local>
2020-12-01 13:51:46 -0800

    Indentation fixes

--- src/components/User.js
+++ src/components/User.js
@@ -3,56 +3,53 @@ import leaderboard from "../assets/leaderboard.png";
 import firebase from '../firebase';
 
 class User extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      topUsersList: {}
-    }
-  }
+   constructor() {
+      super();
+      this.state = {
+         topUsersList: {}
+      }
+   }
 
-  componentDidMount() {
-    var list = [];
-    firebase.database().ref(`users/`)
-      .orderByChild("totalScore")
-      .limitToLast(5)
-      .on('child_added', function(snapshot) {
-        list.push(snapshot.val().username);
+   componentDidMount() {
+      var list = [];
+      firebase.database().ref(`users/`)
+         .orderByChild("totalScore")
+         .limitToLast(5)
+         .on('child_added', function(snapshot) {
+            list.push(snapshot.val().username);
+         });
+      this.setState({
+         topUsersList:list
       });
-    this.setState({
-      topUsersList:list
-    });
-  }
+   }
 
-  render() {
-    var leaderboardList = Object.entries(this.state.topUsersList)
-      .reverse().map((currentVal, i) => {
-        return <li>{i+1}. {currentVal[1]}</li>
-      })
-    var userRank = 0;
-    firebase.database().ref(`users/${this.props.username}`)
-      .once('value', (snapshot) => {
-        userRank = snapshot.val().ranking;
-      })
+   render() {
+      var leaderboardList = Object.entries(this.state.topUsersList)
+         .reverse().map((currentVal, i) => {
+            return <li>{i+1}. {currentVal[1]}</li>
+         })
+      var userRank = 0;
+      firebase.database().ref(`users/${this.props.username}`)
+         .once('value', (snapshot) => {
+            userRank = snapshot.val().ranking;
+         })
 
-    return (
-      <div>
-      <div className="leadertext">
-         <img src={leaderboard} alt="leaderboard" className="leaderimg"/>
-         <span>Leaderboard</span>
-      </div>
-      <ul>
-        <b>
-          {leaderboardList}
-        </b>
-      </ul>
-      <ul>
-        <b>
-          <li>{userRank === 0 ? "" : this.props.username + ": #" + userRank}</li>
-        </b>
-      </ul>
-      </div>
-    );
-
-  }
+      return (
+         <div>
+            <div className="leadertext">
+               <img src={leaderboard} alt="leaderboard" className="leaderimg"/>
+               <span>Leaderboard</span>
+            </div>
+            <ul>
+               <b>{leaderboardList}</b>
+            </ul>
+            <ul>
+               <b>
+                  <li>{userRank === 0 ? "" : this.props.username + ": #" + userRank}</li>
+               </b>
+            </ul>
+         </div>
+      );
+   }
 }
 export default User;
